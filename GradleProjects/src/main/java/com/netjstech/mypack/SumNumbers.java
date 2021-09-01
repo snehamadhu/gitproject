e1528656fb7fce305b5554dcced542e3aa88ed90
Sneha <snehasurve1919@gmail.com>
2021-09-01 13:33:21 +0530

    File changes

--- GradleProjects/src/main/java/com/netjstech/mypack/SumNumbers.java
+++ GradleProjects/src/main/java/com/netjstech/mypack/SumNumbers.java
@@ -2,7 +2,7 @@ package com.netjstech.mypack;
 
 public class SumNumbers {
 	public int sum(int a, int b) {
-		if(a>=10)
+		if(a>=12)
 			throw new IllegalArgumentException("Value should be less than 10");
 		return a+b;
 	}
